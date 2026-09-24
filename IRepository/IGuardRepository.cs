f49310e97ea2dfeac737a8faf24ca5f91358c184
jmcarulasan-dot <jmcarulasan@gmail.com>
2026-09-24 19:07:41 +0800

    Clean Guard and SAO endpoints by role responsibility

--- IRepository/IGuardRepository.cs
+++ IRepository/IGuardRepository.cs
@@ -5,12 +5,8 @@ namespace StudentViolations.API.IRepository
 {
     public interface IGuardRepository
     {
-        Task<ServiceResponse<List<ViolationModel>>> GetViolationsInDateRange(DateTime startDate, DateTime endDate);
-        Task<ServiceResponse<List<ViolationModel>>> GetViolationsByStudentId(string studentNo);
         Task<ServiceResponse<StudentModel>> GetStudentByQrCode(string qrCode);
         Task<ServiceResponse<bool>> RecordViolation(ViolationModel violation);
-        Task<ServiceResponse<List<StudentModel>>> GetAllStudents();
-        Task<ServiceResponse<StudentModel>> GetStudentByStudentNo(string studentNo);
         Task<ServiceResponse<string>> GetUsernameByStudentNo(string studentNo);
     }
 }
