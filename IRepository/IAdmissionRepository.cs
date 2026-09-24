1229918b2c77d9e3d9728f13ec9d7b27d28ce369
jmcarulasan-dot <jmcarulasan@gmail.com>
2026-09-24 17:53:24 +0800

    Restore Admission student registration workflow

--- IRepository/IAdmissionRepository.cs
+++ IRepository/IAdmissionRepository.cs
@@ -5,10 +5,6 @@ namespace StudentViolations.API.IRepository
 {
     public interface IAdmissionRepository
     {
-        Task<ServiceResponse<List<UserModel>>> GetAllUsers();
-        Task<ServiceResponse<UserModel>> GetUserById(int id);
-        Task<ServiceResponse<bool>> UpdateUser(UserModel user);
-        Task<ServiceResponse<bool>> DeleteUser(int id);
-        Task<ServiceResponse<string>> GetStudentQrCode(string studentNo);
+        Task<ServiceResponse<UserModel>> RegisterStudent(UserModel user);
     }
 }
